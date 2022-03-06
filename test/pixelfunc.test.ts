f3f37b7e661b775d8f03bdc82d354e57027badf1
Momtchil Momtchev <momtchil@momtchev.com>
2022-03-06 01:59:31 +0100

    test data type conversion

--- test/pixelfunc.test.ts
+++ test/pixelfunc.test.ts
@@ -1,5 +1,5 @@
 import * as gdal from 'gdal-async';
-import { Float64 as Float64Expression } from 'exprtk.js';
+import { Float64 as Float64Expression, Float32 as Float32Expression } from 'exprtk.js';
 import { toPixelFunc } from '..';
 
 import chai from 'chai';
@@ -65,5 +65,35 @@ describe('toPixelFunc', () => {
         assert.throws(() => {
             ds.bands.get(1).pixels.read(0, 0, ds.rasterSize.x, ds.rasterSize.y);
         }, /wrong number of inputs for Expression/);
+    });
+
+    it('should support data type conversion', () => {
+        const sum2int = new Float32Expression('round(a + b)');
+        gdal.addPixelFunc('sum2int', toPixelFunc(sum2int));
+
+        const vrt = `<VRTDataset rasterXSize="20" rasterYSize="20">
+  <VRTRasterBand dataType="Int32" band="1" subClass="VRTDerivedRasterBand">
+    <Description>CustomPixelFn</Description>
+    <PixelFunctionType>sum2int</PixelFunctionType>
+    <SourceTransferType>Float64</SourceTransferType>
+    <SimpleSource>
+      <SourceFilename relativeToVRT="0">test/data/AROME_T2m_10.tiff</SourceFilename>
+    </SimpleSource>
+    <SimpleSource>
+      <SourceFilename relativeToVRT="0">test/data/AROME_D2m_10.tiff</SourceFilename>
+    </SimpleSource>
+  </VRTRasterBand>
+</VRTDataset>
+`;
+
+        const ds = gdal.open(vrt);
+        assert.equal(ds.bands.count(), 1);
+        const input1 = gdal.open('test/data/AROME_T2m_10.tiff')
+            .bands.get(1).pixels.read(0, 0, ds.rasterSize.x, ds.rasterSize.y);
+        const input2 = gdal.open('test/data/AROME_D2m_10.tiff')
+            .bands.get(1).pixels.read(0, 0, ds.rasterSize.x, ds.rasterSize.y);
+        const result = ds.bands.get(1).pixels.read(0, 0, ds.rasterSize.x, ds.rasterSize.y);
+        for (let i = 0; i < ds.rasterSize.x * ds.rasterSize.y; i += 16)
+            assert.closeTo(result[i], input1[i] + input2[i], 0.5);
     });
 });
